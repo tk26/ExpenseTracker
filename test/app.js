2155b7478b9230a91f44e6a723c20e7c2a234c51
tk26 <me@tejaskumthekar.com>
2017-04-18 21:59:42 -0400

    Added Database and API tests

--- test/app.js
+++ test/app.js
@@ -32,3 +32,35 @@ describe('GET /random-url', () => {
       .expect(404, done);
   });
 });
+
+describe('GET /logout', () => {
+  it('should return 302 Found', (done) => {
+    request(app)
+      .get('/logout')
+      .expect(302, done);
+  });
+});
+
+describe('GET /account', () => {
+  it('should return 302 Found', (done) => {
+    request(app)
+      .get('/account')
+      .expect(302, done);
+  });
+});
+
+describe('GET /expenses', () => {
+  it('should return 302 Found', (done) => {
+    request(app)
+      .get('/expenses')
+      .expect(302, done);
+  });
+});
+
+describe('GET /expense', () => {
+  it('should return 302 Found', (done) => {
+    request(app)
+      .get('/expense')
+      .expect(302, done);
+  });
+});
--- test/models.js
+++ test/models.js
@@ -4,6 +4,7 @@ const sinon = require('sinon');
 require('sinon-mongoose');
 
 const User = require('../models/User');
+const Expense = require('../models/Expense');
 
 describe('User Model', () => {
   it('should create a new user', (done) => {
@@ -98,6 +99,83 @@ describe('User Model', () => {
     User.remove({ email: 'test@gmail.com' }, (err, result) => {
       userMock.verify();
       userMock.restore();
+      expect(err).to.be.null;
+      expect(result.nRemoved).to.equal(1);
+      done();
+    })
+  });
+});
+
+describe('Expense Model', () => {
+  it('should create a new expense', (done) => {
+    const ExpenseMock = sinon.mock(new Expense({ Amount: 2.3, Description: 'Test Expense', CreatedBy: 'test@gmail.com'}));
+    const expense = ExpenseMock.object;
+
+    ExpenseMock
+      .expects('save')
+      .yields(null);
+
+    expense.save(function (err, result) {
+      ExpenseMock.verify();
+      ExpenseMock.restore();
+      expect(err).to.be.null;
+      done();
+    });
+  });
+
+  it('should find expense by id', (done) => {
+    const expenseMock = sinon.mock(Expense);
+    const expectedExpense = {
+      _id: '5700a128bd97c1341d8fb365'
+    };
+
+    expenseMock
+      .expects('findOne')
+      .withArgs({ _id: '5700a128bd97c1341d8fb365' })
+      .yields(null, expectedExpense);
+
+    Expense.findOne({ _id: '5700a128bd97c1341d8fb365' }, (err, result) => {
+      expenseMock.verify();
+      expenseMock.restore();
+      expect(result._id).to.equal('5700a128bd97c1341d8fb365');
+      done();
+    });
+  });
+
+  it('should find expense by email', (done) => {
+    const expenseMock = sinon.mock(Expense);
+    const expectedExpense = {
+      _id: '5700a128bd97c1341d8fb365',
+      CreatedBy: 'test@gmail.com'
+    };
+
+    expenseMock
+      .expects('findOne')
+      .withArgs({ CreatedBy: 'test@gmail.com' })
+      .yields(null, expectedExpense);
+
+    Expense.findOne({ CreatedBy: 'test@gmail.com' }, (err, result) => {
+      expenseMock.verify();
+      expenseMock.restore();
+      expect(result.CreatedBy).to.equal('test@gmail.com');
+      done();
+    });
+  });
+
+  it('should remove expense by id', (done) => {
+    const expenseMock = sinon.mock(Expense);
+    const expectedResult = {
+      nRemoved: 1
+    };
+
+    expenseMock
+      .expects('remove')
+      .withArgs({ _id: '5700a128bd97c1341d8fb365' })
+      .yields(null, expectedResult);
+
+    Expense.remove({ _id: '5700a128bd97c1341d8fb365' }, (err, result) => {
+      expenseMock.verify();
+      expenseMock.restore();
       expect(err).to.be.null;
       expect(result.nRemoved).to.equal(1);
       done();
